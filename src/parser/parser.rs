35e434387dc70a4e608de72048079a910c277116
vaiciits <vaicekauskis.m@gmail.com>
2024-12-26 18:06:55 +0200

    feat: add fn to get to next element

--- src/parser/parser.rs
+++ src/parser/parser.rs
@@ -1,8 +1,16 @@
+use std::collections::HashMap;
+
+pub const CHAR_SPACE: char = ' ';
+pub const CHAR_TAB: char = '\t';
+pub const CHAR_NEWLINE: char = '\n';
+
 pub const CHAR_QUOTE: char = '"';
 
 pub struct Parser<'a> {
     index: usize,
     chars: &'a Vec<char>,
+    length: usize,
+    whitespaces: HashMap<char, ()>,
 }
 
 impl Parser<'_> {
@@ -10,10 +18,32 @@ impl Parser<'_> {
         Parser {
             index: 0,
             chars,
+            length: chars.len(),
+            whitespaces: HashMap::new(),
         }
     }
 
     pub(crate) fn parse(&self) {
 
+    }
+
+    fn get_to_next_element(&mut self) -> usize {
+        while self.index < self.length && self.get_whitespaces().contains_key(&self.chars[self.index]) {
+            self.index += 1;
+        }
+
+        self.index
+    }
+
+    fn get_whitespaces(&mut self) -> HashMap<char, ()> {
+        if (self.whitespaces.len() > 0) {
+            return self.whitespaces.clone()
+        }
+
+        self.whitespaces.insert(CHAR_SPACE, ());
+        self.whitespaces.insert(CHAR_NEWLINE, ());
+        self.whitespaces.insert(CHAR_TAB, ());
+
+        self.whitespaces.clone()
     }
 }
