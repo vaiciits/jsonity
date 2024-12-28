8b3f512252d807108d12767f6ddcf21227ce48dd
vaiciits <vaicekauskis.m@gmail.com>
2024-12-28 19:31:13 +0200

    test: add string parsing test for invalid input

--- src/parser/parser_tests.rs
+++ src/parser/parser_tests.rs
@@ -1,5 +1,6 @@
 #[cfg(test)]
 mod parser_tests {
+    use crate::data_structures::Element;
     use crate::data_structures::Element::String;
     use crate::parser::parser::Parser;
 
@@ -9,16 +10,30 @@ mod parser_tests {
             ("\"\"", ""),
             ("\"foo\"", "foo"),
             ("\"foo\\\\\\\\\\\"bar\"", "foo\\\\\"bar"),
-            ("  \"foo\"  ", "foo")
+            ("  \"foo\"  ", "foo"),
         ];
 
         for (input, expected) in inputs {
-            let chars: Vec<char> = input.chars().collect::<Vec<char>>();
-            let mut parser: Parser = Parser::new(&chars);
-            match parser.parse() {
+            match parse_string(input) {
                 String(string) => assert_eq!(string.value, expected),
                 _ => assert!(false),
             }
         }
+    }
+
+    fn parse_string(input: &str) -> Element {
+        Parser::new(&input.chars().collect()).parse()
+    }
+
+    #[test]
+    #[should_panic]
+    fn parse_with_invalid_string_with_escaped_quote_as_last() {
+        parse_string("\"foo\\\"");
+    }
+
+    #[test]
+    #[should_panic]
+    fn parse_with_invalid_string_with_unclosed_quote() {
+        parse_string("\"foo\\bar");
     }
 }
